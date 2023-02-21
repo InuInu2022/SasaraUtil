a33ada1adbb3bb6f06febed3978d93cf9a86b191
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-02-21 19:21:00 +0900

    Add NLog

--- SasaraUtil/Program.cs
+++ SasaraUtil/Program.cs
@@ -8,29 +8,62 @@
 
 using System;
 using Avalonia;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
-namespace SasaraUtil
+namespace SasaraUtil;
+
+public static class Program
 {
-	public static class Program
-	{
-		// Initialization code. Don't use any Avalonia, third-party APIs or any
-		// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-		// yet and stuff might break.
-		[STAThread]
-		public static void Main(string[] args) =>
-			BuildAvaloniaApp().
-			StartWithClassicDesktopLifetime(args);
+	private static readonly NLog.Logger Logger
+		= NLog.LogManager.GetCurrentClassLogger();
 
-		// Avalonia configuration, don't remove; also used by visual designer.
-		public static AppBuilder BuildAvaloniaApp() =>
-			AppBuilder.Configure<App>()
-				.UsePlatformDetect()
-				.LogToTrace()
-				.With(new Win32PlatformOptions
-					{
-						UseWindowsUIComposition = true
-					}
-				)
-				;
+	// Initialization code. Don't use any Avalonia, third-party APIs or any
+	// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+	// yet and stuff might break.
+	[STAThread]
+	public static void Main(string[] args)
+	{
+		try
+		{
+			InitLogger();
+			Logger.Info("App starting...");
+		  	BuildAvaloniaApp().
+				StartWithClassicDesktopLifetime(args);
+		}
+		catch (Exception ex)
+		{
+		   Logger.Error(ex, "Main App Error!");
+		}
+	}
+
+	// Avalonia configuration, don't remove; also used by visual designer.
+	public static AppBuilder BuildAvaloniaApp() =>
+		AppBuilder.Configure<App>()
+			.UsePlatformDetect()
+			.LogToTrace()
+			.With(new Win32PlatformOptions
+				{
+					UseWindowsUIComposition = true
+				}
+			)
+			;
+
+	private static void InitLogger()
+	{
+		var config = new LoggingConfiguration();
+
+		var fileTarget = new FileTarget();
+		config.AddTarget("file", fileTarget);
+
+		fileTarget.Name = "f";
+		fileTarget.FileName = "${basedir}/logs/${shortdate}.log";
+		fileTarget.Layout = "${longdate} [${uppercase:${level}}] ${message}";
+
+		var rule1 = new LoggingRule("*", LogLevel.Info, fileTarget);
+		config.LoggingRules.Add(rule1);
+
+		LogManager.Configuration = config;
 	}
 }
