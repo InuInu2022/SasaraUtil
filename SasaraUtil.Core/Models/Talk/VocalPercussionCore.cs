3e5e9a898194a524b9bacf3e1dd0c4a095d52708
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-03-04 18:26:15 +0900

    feat: Add VocalPercussion beta

--- SasaraUtil.Core/Models/Talk/VocalPercussionCore.cs
+++ SasaraUtil.Core/Models/Talk/VocalPercussionCore.cs
@@ -1,8 +1,376 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentCeVIOWrapper.Common;
+using LibSasara;
+using LibSasara.Builder;
+using LibSasara.Model;
+using MathNet.Numerics.Statistics;
 
 namespace SasaraUtil.Models.Talk;
 
-public class VocalPercussionCore
-{
+/**
+base source code from
+https://github.com/InuInu2022/LibSasara/blob/master/sample/csharp/SongToTalk/Program.cs
+**/
 
+public class VocalPercussionCore{
+	private static Process? process;
+	private FluentCeVIO? fcw;
+	private CeVIOFileBase? srcCcs;
+	private CeVIOFileBase? exportCcs;
+	private SongUnit? song;
+	private string? src;
+	private string? dist;
+
+	public async Task ExecuteAsync(
+		string src,
+		string dist,
+		string castName,
+		CeVIOFileBase template,
+		string TTS,
+		bool stretch
+	)
+	{
+		if(!File.Exists(src)){
+			throw new FileNotFoundException($"src:{src} is not found");
+		}
+
+		this.src = src;
+
+		/*
+		if(!File.Exists(dist)){
+			throw new FileNotFoundException($"{dist} is not found");
+		}*/
+
+		this.dist = dist;
+
+		/*
+		if(!string.IsNullOrEmpty(template) && !File.Exists(template)){
+			throw new FileNotFoundException($"template:{template} is not found");
+		}*/
+
+		await AwakeAsync(TTS);
+
+		fcw = await StartTalkAsync(TTS);
+
+		var casts = await fcw.GetAvailableCastsAsync();
+		await fcw.CreateParam()
+			.Cast(casts[0])
+			.SendAsync();
+
+		await fcw.SpeakAsync(
+			"CeVIOトークでボイパロイドします。"
+		);
+
+		if(!casts.Contains(castName)){
+			process!.Kill();
+			throw new ArgumentException($"{castName}は存在しないキャスト名です。");
+		}
+
+		await fcw.SetCastAsync(castName);
+		await fcw.SpeakAsync($"キャストを{castName}に切り替えました。");
+
+		srcCcs = await SasaraCcs.LoadAsync(src);
+		song = srcCcs
+			.GetUnits(Category.SingerSong)
+			.OfType<SongUnit>()
+			.First();
+		var tempo = song.Tempo;
+		var notes = song.Notes;
+		var groupId = Guid.NewGuid();
+
+		var c = await fcw
+			.GetComponentsAsync();
+
+		var castId = await fcw.GetCastIdAsync(castName);
+		//var castId = string.Join("-",c[0].Id.Split("-")[0..3]);
+
+		//var path = string.IsNullOrEmpty(template) ?
+		//	dist : template;
+		exportCcs = template;//await SasaraCcs.LoadAsync(path);
+
+		//if(Path.GetExtension(path) == "ccst"){
+		//	exportCcs.RemoveAllGroups();
+		//}
+
+		exportCcs.AddGroup(
+			groupId,
+			Category.TextVocal,
+			$"ボイパ_{castName}_{srcCcs.GetTrackSet(song.Group).group.Attribute("Name")?.Value}"
+		);
+
+		var cacheTones = new Dictionary<string, double>();
+		var cachePhonemes = new Dictionary<string, ReadOnlyCollection<FluentCeVIOWrapper.Common.Models.PhonemeData>>();
+
+		var units = await notes
+			.ToAsyncEnumerable()
+			.Where(v => !string.IsNullOrEmpty(v.Lyric))
+			.SelectAwait(async (v, i) =>
+			{
+				//"ー"
+				return await ReplaceProlongedMarkAsync(v, i, notes);
+			})
+			.SelectAwait(async (v, i) =>
+			{
+				//"っ"
+				return await ReplaceCloseConsonantAsync(v, i, notes);
+			})
+			/*.SelectAwait(async v =>
+			{
+				return await CulcSpeedAsync(v, tempo);
+			})*/
+			.SelectAwait(async v =>
+			{
+				//Stretch option
+				//APIとccsで速さ指定単位が異なるので要計算
+				//API: 0.2 ～ 5.0 (cen:1.0)
+				//ccs: 0 ～ 100 (cen: 50)
+
+				if(stretch)
+				{
+					//TODO: stretch
+					await fcw.SetSpeedAsync((uint)50);
+				}else{
+					await fcw.SetSpeedAsync(50);
+				}
+
+				//await fcw.SpeakAsync(v.Lyric!);
+				var culc = await fcw
+					.GetTextDurationAsync(v.Lyric!);
+				//Console.WriteLine($"{v.cast}[{v.Lyric}] note:{v.noteLen}, talklen:{v.len}, rate:{v.rate}, culc:{culc}");
+
+				//pitch
+
+				//target freq.
+				var tFreq = LibSasara.SasaraUtil.OctaveStepToFreq(v.PitchOctave, v.PitchStep);
+				//Console.WriteLine($"Target Freq. {tFreq}");
+				await fcw.SetToneAsync(50);
+				var isCachedLyric = cacheTones.ContainsKey(v.Lyric!);
+
+				double avgFreq;
+				if(isCachedLyric)
+				{
+					avgFreq = cacheTones[v.Lyric!];
+				}
+				else{
+					var wp = await EstimateAsync(v.Lyric!);
+					avgFreq = wp.F0?.Where(f => f > 0).Mean() ?? 0;
+					cacheTones
+						.Add(v.Lyric!, avgFreq);
+				}
+
+				Console.WriteLine($" avg:{avgFreq}, target:{tFreq}");
+
+				var isCachedPhoneme = cachePhonemes
+					.ContainsKey(v.Lyric!);
+				var ph = isCachedPhoneme ?
+					cachePhonemes[v.Lyric!] :
+					await fcw.GetPhonemesAsync(v.Lyric!);
+				if (!isCachedPhoneme)
+				{
+					cachePhonemes[v.Lyric!] = ph;
+				}
+
+				var phs = ph
+					//.Select(p => p.Phoneme)
+					.Select((p, i) =>
+					{
+						return new TalkPhoneme
+						{
+							Index = i,
+							Data = p.Phoneme,
+							//Speed = 0,
+							Tone = TalkPhoneme.CulcTone(avgFreq, tFreq)
+						};
+					})
+					;
+
+				Debug.WriteLine($"phs:{phs.Count()}");
+
+				//create units
+				return TalkUnitBuilder
+					.Create(
+						exportCcs,
+						LibSasara.SasaraUtil
+							.ClockToTimeSpan(tempo, v.Clock),
+						TimeSpan.FromSeconds(culc),
+						castId,
+						v.Lyric ?? ""
+					)
+					.Group(groupId)
+					.Speed(1m)	//TODO:option
+					.Phonemes(phs)
+					.Build();
+
+				//return (targetRate:r, setFreq);
+			})
+			.ToListAsync()
+			;
+		await exportCcs
+			.SaveAsync($"{this.dist}");
+	}
+
+	/// <summary>
+    /// 「っ」を置き換える
+    /// </summary>
+	/// <remarks>
+	/// 「っ」単体の場合は直前の母音を調べて、先頭にくっつける。
+    /// 見つからない場合は「あっ」。
+	/// </remarks>
+    /// <param name="v"></param>
+    /// <param name="i"></param>
+    /// <param name="notes"></param>
+    /// <returns></returns>
+	private async ValueTask<Note> ReplaceCloseConsonantAsync(Note v, int i, List<Note> notes)
+	{
+		//TODO:「っ」で始まる複数文字の歌詞の場合の対応
+		if (v.Lyric != "っ")
+		{
+			return v;
+		}
+
+		var lNote = notes
+			.GetRange(0, i)
+			.Last(n =>
+				n.Lyric is not "っ");
+		var ph = await fcw!.GetPhonemesAsync(lNote.Lyric!);
+		var last = ph
+			.Last(v =>
+				v.Phoneme is not "sil"
+				and not "pau");
+		var nLyric = last.Phoneme switch
+		{
+			"a" => "あ",
+			"i" => "い",
+			"u" => "う",
+			"e" => "え",
+			"o" => "お",
+			_ => "あ"
+		};
+		v.Lyric = $"{nLyric}っ";
+		return v;
+	}
+
+	/// <summary>
+	/// 「ー」を置き換える
+	/// </summary>
+	/// <remarks>
+	/// 直前の母音を探してその音で置き換える。
+	/// もしなければ「あ」。
+	/// </remarks>
+	/// <param name="v"></param>
+	/// <param name="i"></param>
+	/// <param name="notes"></param>
+	/// <returns></returns>
+	private async ValueTask<Note> ReplaceProlongedMarkAsync(Note v, int i, List<Note> notes)
+	{
+		if (v.Lyric != "ー")
+		{
+			return v;
+		}
+
+		var nLyric = "あ";
+		var lNote = notes
+			.GetRange(0, i)
+			.Last(n =>
+				n.Lyric is not "ー"
+					and not null
+					and not "");
+
+		var ph = await fcw!.GetPhonemesAsync(lNote.Lyric!);
+
+		var last = ph
+			.Last(v =>
+				v.Phoneme is not "sil"
+				and not "pau");
+		nLyric = last.Phoneme switch
+		{
+			"a" => "あ",
+			"i" => "い",
+			"u" => "う",
+			"e" => "え",
+			"o" => "お",
+			_ => "あ"
+		};
+
+		v.Lyric = nLyric;
+		return v;
+	}
+
+	public static void Finish()
+	{
+		process?.Kill();
+	}
+
+	static async ValueTask AwakeAsync(string TTS)
+	{
+		var ps = new ProcessStartInfo()
+		{
+			FileName = Path.Combine(
+				AppDomain.CurrentDomain.BaseDirectory,
+				@".\server\FluentCeVIOWrapper.Server.exe"
+			),
+			Arguments = $"-cevio {TTS}",
+			CreateNoWindow = true,
+			ErrorDialog = true,
+			UseShellExecute = true,
+			//RedirectStandardOutput = true,
+		};
+		ps.WorkingDirectory = Path.GetDirectoryName(ps.FileName);
+		try
+		{
+			process = Process.Start(ps);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"Error {e}");
+		}
+
+		System.Console.WriteLine("awaked.");
+		await Task.Delay(2000);
+	}
+
+	public async Task<FluentCeVIO> StartTalkAsync(string TTS){
+		var product = TTS switch
+		{
+			"CeVIO_AI" => Product.CeVIO_AI,
+			_ => Product.CeVIO_CS
+		};
+		return await FluentCeVIO
+			.FactoryAsync(product: product);
+	}
+
+	public async ValueTask<WorldParam> EstimateAsync(string serifText)
+	{
+		var tempName = Path.GetTempFileName();
+
+		var resultOutput = await fcw!.OutputWaveToFileAsync(serifText, tempName);
+		if (!resultOutput)
+		{
+			var msg = $"Faild to save temp file!:{tempName}";
+			throw new Exception(msg);
+		}
+
+		var (fs, nbit, len, x) = await WorldUtil.ReadWavAsync(tempName);
+
+		//Console.WriteLine((fs, nbit, len, x));
+		var parameters = new WorldParam(fs);
+		//ピッチ推定
+		parameters = await WorldUtil.EstimateF0Async(
+			x,
+			len,
+			parameters
+		);
+		if (tempName != null && File.Exists(tempName))
+		{
+			await Task.Run(()=> File.Delete(tempName));  //remove temp file
+		}
+
+		return parameters;
+	}
 }
--- SasaraUtil.UI/ViewModels/Talk/VocalPercussionViewModel.cs
+++ SasaraUtil.UI/ViewModels/Talk/VocalPercussionViewModel.cs
@@ -11,7 +11,7 @@ using Epoxy;
 using Epoxy.Synchronized;
 
 using LibSasara;
-
+using SasaraUtil.Models.Talk;
 using SasaraUtil.ViewModels.Utility;
 
 namespace SasaraUtil.ViewModels.VocalPercussion;
@@ -26,8 +26,9 @@ public class VocalPercussionViewModel
 	public string? TargetFileName { get; set; }
 	public ObservableCollection<CcsTrackViewModel> CcsTrackData { get; set; }
 	public Command ResetFiles { get; }
-
 	public Command? SaveFile { get; set; }
+	public Command? SelectFile { get; set; }
+	public bool IsOpenWithCeVIO { get; set; }
 
 	public VocalPercussionViewModel()
 	{
@@ -40,6 +41,9 @@ public class VocalPercussionViewModel
 		SaveFile = CommandFactory
 			.Create(SaveFileAsync);
 
+		SelectFile = CommandFactory
+			.Create(LoadFileAsync);
+
 		_notify = Utility
 			.MainWindowUtil
 			.GetNotifyManager();
@@ -47,9 +51,11 @@ public class VocalPercussionViewModel
 
 	private async ValueTask SaveFileAsync()
 	{
+		IsConvertable = false;
 		if(DroppedFiles is null || DroppedFiles.Count == 0){
 			_notify?
 				.Warn("ファイルエラー", "変換するファイルがみつかりません");
+			IsConvertable = true;
 			return;
 		}
 
@@ -60,6 +66,7 @@ public class VocalPercussionViewModel
 		if(path is null){
 			_notify?
 				.Warn("ファイルエラー", "変換するファイルがみつかりません");
+			IsConvertable = true;
 			return;
 		}
 
@@ -75,13 +82,14 @@ public class VocalPercussionViewModel
 				Path.GetDirectoryName(path),
 			Filters = new(){ filter },
 			InitialFileName =
-				Path.ChangeExtension(fileName, "splitted.ccs"),
+				Path.ChangeExtension(fileName, "voiceperc.ccs"),
 		};
 
 		var saveDir = string.Empty;
 		var mainWin = Utility.MainWindowUtil.GetWindow();
 		if(mainWin is null){
 			_notify?.Dismiss(loading!);
+			IsConvertable = true;
 			return;
 		}else{
 			saveDir = await d.ShowAsync(mainWin);
@@ -91,20 +99,79 @@ public class VocalPercussionViewModel
 		{
 			//canceled
 			_notify?.Dismiss(loading!);
+			IsConvertable = true;
 			return;
 		}
 
 		IsConvertable = false;
 
-		var ccs = await SasaraCcs.LoadAsync(path);
+		//var ccs = await SasaraCcs.LoadAsync(path);
 		//await Models.VocalPercussion.VocalPercussion
 		//	.SplitTrackByCastAsync(ccs);
 
-		await ccs.SaveAsync(saveDir);
+		var vp = new VocalPercussionCore();
+		var template = await Models.TrackTemplateLoader.LoadProjectAsync();
+		try
+		{
+			await vp.ExecuteAsync(
+				path,
+				saveDir,
+				"タカハシ",
+				template,
+				"CeVIO_CS",
+				false
+			);
+		}
+		catch (Exception e)
+		{
+			_notify?.Dismiss(loading!);
+			_notify?
+				.Warn("ファイルエラー", $"変換に失敗しました。{e.Message}");
+			VocalPercussionCore.Finish();
+			IsConvertable = true;
+			return;
+		}
+		finally{
+			VocalPercussionCore.Finish();
+		}
 
 		_notify?.Dismiss(loading!);
 		_notify?.Info("保存成功", "保存しました", true);
 		IsConvertable = true;
+
+		if(IsOpenWithCeVIO){
+			Models.ProcessManager
+				.Open(saveDir);
+		}
+	}
+
+	private async ValueTask LoadFileAsync(){
+		var filter = new FileDialogFilter
+		{
+			Extensions = new() { "ccs", "ccst" }
+		};
+		var d = new OpenFileDialog()
+		{
+			Title = "ボイパさせるソングデータを含むccsファイルを選んでください",
+			AllowMultiple = false,
+			Filters = new(){ filter },
+		};
+
+		string[]? songCcs;
+		var mainWin = MainWindowUtil.GetWindow();
+		if(mainWin is null){
+			return;
+		}else{
+			songCcs = await d
+				.ShowAsync(mainWin);
+		}
+
+		if (songCcs is null)
+		{
+			return;
+		}
+
+		await ProcessingFileAsync(new ReadOnlyCollection<string>(songCcs));
 	}
 
 	public async ValueTask DropFileEventAsync(DragEventArgs e)
@@ -115,15 +182,23 @@ public class VocalPercussionViewModel
 			return;
 		}
 
+		await ProcessingFileAsync(DropUtil.GetFileNames(e));
+	}
+
+	private async ValueTask ProcessingFileAsync(
+		ReadOnlyCollection<string> pathes
+	)
+	{
 		var loading = _notify!
 			.Loading("解析中", "ファイルを解析しています...");
 
-		var list = DropUtil.GetFileNames(e);
+		var list = pathes;
 		var list2 = await Task.Run(() =>
 		{
 			return list
 				.Where(v =>
-					Path.GetExtension(v) is ".ccs");
+					Path.GetExtension(v) is ".ccs" or
+					".ccst");
 		});
 
 		if(!list2.Any())
@@ -137,23 +212,7 @@ public class VocalPercussionViewModel
 		TargetFileName = Path.GetFileName(list2.First());
 		var ccs = await SasaraCcs.LoadAsync(list2.First());
 
-		/*var casts = await Models.VocalPercussion.VocalPercussion
-			.GetCastsByTrackAsync(ccs);
-		var tracks = casts.Select(v => (v.Name, v.GroupId));
-
-		var list3 = casts
-			.SelectMany(v => v.Units.Select(v => v))
-			.Select(v =>
-				new CcsTrackViewModel(
-					tracks
-						.First(t => t.GroupId == v.Group)
-						.Name,
-					v.CastId,
-					v.Text
-				)
-			);
-		CcsTrackData = new(list3);
-		*/
+		//TODO:here
 
 		IsConvertable = true;
 		_notify!.Dismiss(loading);
